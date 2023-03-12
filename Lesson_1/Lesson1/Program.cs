10de103bd28c32a66617ee3bd5b571d8ea1d51aa
Nasyutka <filippova.nastasia2001@mail.ru>
2023-03-12 22:02:36 +0700

    Initial project
    Add homework for lesson1

---
+++ Lesson_1/Lesson1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Lesson1
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("" + name + ", Hi!");
+        }
+    }
+}
